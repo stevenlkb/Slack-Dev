5be43e2eaedab10dd98396f7ef9dfeae1ba044ce
Steven Lim <lim.kiatboon@gmail.com>
2023-04-04 08:58:51 +0800

    req.body update

--- purchase.js
+++ purchase.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post('/purchase', async (req, res) => {
-  console.log(req);
+  console.log(req.body);
   res.send('OK');
 });
 
